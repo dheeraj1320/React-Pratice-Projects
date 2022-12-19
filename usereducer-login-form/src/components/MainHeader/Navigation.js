7dc2d3ca542181672a05be41fc3d3285b2ee3696
Dheeraj Kumar Manwani <dheerajmanwani2000@gmail.com>
2022-12-19 23:49:14 +0530

    context api

--- usereducer-login-form/src/components/MainHeader/Navigation.js
+++ usereducer-login-form/src/components/MainHeader/Navigation.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const contx = useContext(AuthContext);
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {contx.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {contx.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {contx.isLoggedIn && (
           <li>
             <button onClick={props.onLogout}>Logout</button>
           </li>
