f504a2b81736a33514ae554e1021da0c32d0159f
Dheeraj Kumar Manwani <dheerajmanwani2000@gmail.com>
2023-01-19 13:36:57 +0530

    router, link, children

--- React router practice/my-app/src/App.js
+++ React router practice/my-app/src/App.js
@@ -1,12 +1,33 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+]);
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
 
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ React router practice/my-app/src/pages/Products.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const ProductsPage = () => {
+  return (
+    <>
+      <h1>The products page</h1>
+      <Link to="/">back</Link>
+    </>
+  );
+};
+
+export default ProductsPage;
