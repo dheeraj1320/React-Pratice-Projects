2517c76c004d0802b8121369033f1c1c7e5808e7
Dheeraj Kumar Manwani <dheerajmanwani2000@gmail.com>
2023-01-21 14:06:21 +0530

    nested layout routes

--- 12-adv-starting-project/frontend/src/App.js
+++ 12-adv-starting-project/frontend/src/App.js
@@ -3,6 +3,7 @@ import EditEventPage from "./pages/EditEvent";
 
 import EventDetailPage from "./pages/EventDetail";
 import EventsPage from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
 import NewEventPage from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
@@ -13,10 +14,16 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "events", element: <EventsPage /> },
-      { path: "events/:eventId", element: <EventDetailPage /> },
-      { path: "events/new", element: <NewEventPage /> },
-      { path: "events/:eventId/edit", element: <EditEventPage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          { index: true, element: <EventsPage /> },
+          { path: ":eventId", element: <EventDetailPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":eventId/edit", element: <EditEventPage /> },
+        ],
+      },
     ],
   },
 ]);
